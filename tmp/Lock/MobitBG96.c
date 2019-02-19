46c4f315f59908cdcfb54be54685d12ff9c8c0a0
abc3240660 <damon@damon.com>
2019-02-19 22:30:58 +0800

    Add BG96 Demo for TCPIP / GNSS / HTTPClient

--- tmp/Lock/MobitBG96.c
+++ tmp/Lock/MobitBG96.c
@@ -1986,3 +1986,186 @@ SSL_Socket_Event_t WaitCheckSSLSocketEvent(char *event, unsigned int timeout)
     }
     return 0;
 }
+
+int TcpClientDemo(void)
+{
+    const char APN[] = "hologram";
+    const char tcp_ip[] = "mbed.org";
+    const int tcp_port = 80;
+    const char send_data[] = "GET /media/uploads/mbed_official/hello.txt HTTP/1.0\r\n\r\n";
+    unsigned int comm_pdp_index = 2;  // The range is 1 ~ 16
+    unsigned int comm_socket_index = 2;  // The range is 0 ~ 11
+    Socket_Type_t socket = TCP_CLIENT;
+        
+    // Setup
+    printf("This is the Mobit Debug Serial!\n");
+    delay_ms(1000);
+    while(!InitModule());
+
+    SetDevCommandEcho(false);
+
+    char inf[64];
+    if(GetDevInformation(inf)){
+        printf("Dev Info: %s!\n", inf);
+    }
+
+    char apn_error[64];
+    while (!InitAPN(comm_pdp_index, APN, "", "", apn_error)){
+        printf("apn_error :\n", apn_error);
+    }
+    printf("apn_error :\n", apn_error);
+
+    while (!OpenSocketService(comm_pdp_index, comm_socket_index, socket, tcp_ip, tcp_port, 0, BUFFER_MODE)){
+        printf("Open Socket Service Fail!\n");
+    }
+    printf("Open Socket Service Success!\n");
+
+    if(SocketSendData(comm_socket_index, socket, send_data, "", "")){
+        printf("Socket Send Data Success!\n");
+    }
+
+    char m_event[16];
+    unsigned int index;
+    char recv_data[128];
+
+    while(1) {
+        Socket_Event_t ret = WaitCheckSocketEvent(m_event, 2);
+
+        switch(ret)
+        {
+            case SOCKET_CLOSE_EVENT:
+                index = atoi(m_event);
+                if(CloseSocketService(index)){
+                    printf("Close Socket Success!\n");
+                }
+                break;
+            case SOCKET_RECV_DATA_EVENT:
+                index = atoi(m_event);
+                if (SocketRecvData(index, 128, socket, recv_data)){
+                    printf("Socket Recv Data Success!\n");
+                    printf("recv_data: %s\n", recv_data);
+                }
+                break;
+            case SOCKET_PDP_DEACTIVATION_EVENT:
+                index = atoi(m_event);
+                if(DeactivateDevAPN(index)){
+                    printf("Please reconfigure APN!\n");
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    return 0;
+}
+
+int HttpClientDemo(void)
+{
+    const char APN[] = "hologram";
+    const char http_url[] = "http://www.baidu.com";   //"http://app.tongxinmao.com:89/app/api/ip";
+    unsigned int comm_pdp_index = 1;    // The range is 1 ~ 16
+    HTTP_Body_Data_Type_t  http_type = APPLICATION_X_WWW_FORM_URLENCODED;
+        
+    // Setup
+    printf("This is the Mobit Debug Serial!\n");
+    delay_ms(1000);
+    while(!InitModule());
+
+    SetDevCommandEcho(false);
+
+    char inf[64];
+    if(GetDevInformation(inf)){
+        printf("Dev Info: %s!\n", inf);
+    }
+
+    char apn_error[64];
+    while (!InitAPN(comm_pdp_index, APN, "", "", apn_error)){
+        printf("apn_error :\n", apn_error);
+    }
+
+    printf("apn_error :\n", apn_error);
+
+    while(!SetHTTPConfigParameters(comm_pdp_index, false, false, http_type)){
+        printf("Config the HTTP Parameter Fail!\n");
+        int e_code;
+        if (returnErrorCode(e_code)){
+            printf("ERROR CODE: %s\n", e_code);
+            printf("Please check the documentation for error details.\n");
+            while(1);
+        }
+    }
+    printf("Config the HTTP Parameter Success!\n");
+
+    while(!HTTPURL(http_url, WRITE_MODE)){
+        printf("Set the HTTP URL Fail!\n");
+        int e_code;
+        if (returnErrorCode(e_code)){
+            printf("ERROR CODE: %s\n", e_code);
+            printf("Please check the documentation for error details.\n");
+            while(1);
+        }
+    }
+    printf("Set the HTTP URL Success!\n");
+
+    const char recv_file[] = "http_read.txt";
+
+    while(1) {
+        if(!HTTPGET(80)){
+            printf("HTTP GET Success!\n");
+            int e_code;
+            if (returnErrorCode(e_code)){
+                printf("ERROR CODE: %s\n", e_code);
+                printf("Please check the documentation for error details.\n");
+                while(1);
+            }
+        }
+        printf("HTTP GET Success!\n");
+
+        if(HTTPReadToFile(recv_file, 80)){
+            printf("HTTP Read to File Success!\n");
+        }
+    }
+    return 0;
+}
+
+int GNSSDemo(void)
+{
+    GNSS_Work_Mode_t mode = STAND_ALONE;
+
+    printf("This is the Mobit Debug Serial!\n");
+    delay_ms(1000);
+    while(!InitModule());
+
+    SetDevCommandEcho(false);
+
+    const char inf[64];
+    if(GetDevInformation(inf)){
+        printf("Dev Info: %s!\n", inf);
+    }
+
+    while (!TurnOnGNSS(mode, WRITE_MODE)){
+        printf("Open the GNSS Function Fali!\n");
+        if(TurnOnGNSS(mode, READ_MODE)){
+            printf("The GNSS Function is Opened!\n");
+            TurnOffGNSS();
+        }
+    }
+    printf("Open the GNSS Function Success!\n");
+    
+    const char gnss_posi[128];
+
+    while(1) {
+        while (!GetGNSSPositionInformation(gnss_posi)){
+            printf("Get the GNSS Position Fail!\n");
+            int e_code;
+            if (returnErrorCode(e_code)){
+                printf("ERROR CODE: %d\n", e_code);
+                printf("Please check the documentation for error details.\n");
+            }
+            delay_ms(5000);
+        }
+        printf("Get the GNSS Position Success!\n");
+        printf("gnss_posi = %s\n", gnss_posi);
+    }
+    return 0;
+}
